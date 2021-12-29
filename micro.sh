66789a05ab9eb553131398ef6d3022150acad6f9
Owen Valentine <owenvalentine@gmail.com>
2021-12-29 17:53:01 +0200

    Quiet wget

--- micro.sh
+++ micro.sh
@@ -4,7 +4,9 @@ MICRO_VERSION="${1:-2.0.10}"
 MICRO_OS="${2:-linux64}"
 export MICRO_VERSION MICRO_OS
 
-wget "https://github.com/zyedidia/micro/releases/download/v$MICRO_VERSION/micro-$MICRO_VERSION-$MICRO_OS.tar.gz"
+wget -q "https://github.com/zyedidia/micro/releases/download/v$MICRO_VERSION/micro-$MICRO_VERSION-$MICRO_OS.tar.gz"
 tar xf "micro-$MICRO_VERSION-$MICRO_OS.tar.gz" "micro-$MICRO_VERSION/micro"
 sudo mv "micro-$MICRO_VERSION/micro" /usr/local/bin/micro
 rm -r "micro-$MICRO_VERSION" "micro-$MICRO_VERSION-$MICRO_OS.tar.gz"
+
+echo "Done!"
