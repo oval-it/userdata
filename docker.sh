139ba86490baa8fece87b7c32b61e8bd5958e76d
Owen Valentine <owenvalentine@gmail.com>
2021-12-30 21:57:10 +0200

    Fixy

--- docker.sh
+++ docker.sh
@@ -3,8 +3,8 @@
 # Install prerequisites
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 
-# Add Docker repo, install
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+# Add Docker repo, installo
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
