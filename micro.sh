0b67cc3afd404c0f9b9bd84ea749255f1ca16b66
Owen Valentine <owenvalentine@gmail.com>
2021-12-30 21:51:34 +0200

    And then do the rest

--- micro.sh
+++ micro.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+# Set version/OS
 MICRO_VERSION="${1:-2.0.10}"
 MICRO_OS="${2:-linux64}"
 export MICRO_VERSION MICRO_OS
 
+# Get binary
+cd /tmp || exit
 wget -q "https://github.com/zyedidia/micro/releases/download/v$MICRO_VERSION/micro-$MICRO_VERSION-$MICRO_OS.tar.gz"
 tar xf "micro-$MICRO_VERSION-$MICRO_OS.tar.gz" "micro-$MICRO_VERSION/micro"
 sudo mv "micro-$MICRO_VERSION/micro" /usr/local/bin/micro
+
+# Get config
+git clone --quiet https://github.com/chessmango/settings.git
+mkdir -p ~/.config
+cp -r settings/micro ~/.config/micro
+sudo mkdir -p ~root/.config
+sudo cp -r settings/micro ~root/.config/micro
+sudo chown -R root:root ~/root/.config/micro
+
+# Cleanup
 rm -r "micro-$MICRO_VERSION" "micro-$MICRO_VERSION-$MICRO_OS.tar.gz"
+rm -r settings
 
 echo "Done!"
